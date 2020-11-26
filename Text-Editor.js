2f44c5e5a930e203f55879bba223e3534299b8bc
GamingThistle <36027062+CALVlN@users.noreply.github.com>
2020-11-25 16:45:52 -0800

    Added a div with all of the words that will be highlighted.
    Added a div with all of the words that will be highlighted. Named all the divs. Deleted reset button. Added save and load button.

--- Text-Editor.js
+++ Text-Editor.js
@@ -2,7 +2,7 @@ window.onload = function () {
     if (localStorage.getItem('text-in-editor') !== null) {
         document.getElementById('telling-words-editor').innerHTML = localStorage.getItem('text-in-editor');
         /* FIX THIS #1 */
-        fixCursorPositionOnLoad();
+        fixCursorPosition();
     }
 
     document.addEventListener('keyup', function (e) {
@@ -24,9 +24,9 @@ window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
 
-function clearText() {
-    /* Clears the text in the div. */
-    document.getElementById('telling-words-editor').innerHTML = '';
+function saveText() {
+    // Sets the local storage key named 'savedText' to the html in the editor.
+    localStorage.setItem('savedText', document.getElementById('telling-words-editor').innerHTML);
 
     /* Sets focus to the div. */
     document.getElementById('telling-words-editor').focus();
@@ -35,29 +35,79 @@ function clearText() {
     localStorage.setItem('text-in-editor', document.getElementById('telling-words-editor').innerHTML);
 }
 
+/* Reset the div to before the clear button was pressed. */
+function loadText() {
+    // sets variable undoClearText to the local storage variable 'undoClear'
+    var savedText = localStorage.getItem('savedText');
+
+    // Sets editor html to undoClearText variable.
+    document.getElementById('telling-words-editor').innerHTML = savedText;
+
+    // Sets focus to the div.
+    document.getElementById('telling-words-editor').focus();
+
+    // Sets the data saved on reload to what is in the div.
+    localStorage.setItem('text-in-editor', document.getElementById('telling-words-editor').innerHTML);
+
+    /* Call these functions on button press. */
+    highlightWords();
+    fixCursorPosition();
+}
+
 /* FIX THIS #1 */
 /* Fix the cursor position in the editable div element when the page loads. */
-function fixCursorPositionOnLoad() {
+function fixCursorPosition() {
     // select all the content in the element
     document.execCommand('selectAll', false, null);
     // collapse selection to the end
     document.getSelection().collapseToEnd();
 }
 
+function wordsToHighlight() {
+    var divText = document.getElementById('words-to-highlight-div').innerText;
+
+    var tokenized = tokenize(divText, { word: /\w+/, whitespace: /\s+/, punctuation: /[^\w\s]/ }, 'invalid');
+
+    var filteredTokens = [];
+    var words = [];
+
+/*    tokenized.forEach(function (token) {
+        if (token.type === 'word') {
+            filteredTokens.push(token.token);
+        }
+    });*/
+
+    filteredTokens = tokenized.filter(function (arrayItem) {
+        console.log(arrayItem.type === 'word');
+        return arrayItem.type === 'word';
+    });
+
+    words = filteredTokens.map(function (arrayItem) {
+        return arrayItem.token;
+    });
+
+    console.log(tokenized);
+    console.log(words);
+    return words;
+}
+
 function highlightWords() {
     var textInEditor = document.getElementById('telling-words-editor').innerText;
     var locationsOfWords = [];
     var result = '';
-    var highlightTheseWords = ['the', 'locations', 'this'];
+    var highlightTheseWords = [];
     
     var tokenized = tokenize(textInEditor, { word: /\w+/, whitespace: /\s+/, punctuation: /[^\w\s]/ }, 'invalid');
-    
+
+    /* DEBUGGING CODE */
+    highlightTheseWords = wordsToHighlight();
+
 /* This stores the position of every word that will be highlighted in an array. */
     highlightTheseWords.forEach(function (word) {
         locationsOfWords = locationsOfWords.concat(getAllIndexes(tokenized, word));
     });
 
-    /* This line makes whatever this is into a string. */
+/* This line makes whatever this is into a string. */
     tokenized.forEach(function (tokenObject, index) {
         if (locationsOfWords.includes(index)) {
             result += '<b class="general-highlight">' + tokenObject.token + '</b>';
@@ -66,13 +116,11 @@ function highlightWords() {
             result += tokenObject.token;
         }
     });
+
     /* This line replaces the div text with the result variable. NOTE: If the result variable has white space at the end, that is not translated into the div. */
     document.getElementById('highlighted-words').innerHTML = result;
     /* This line sets the data saved on reload to the results variable.*/
     localStorage.setItem('text-in-editor', document.getElementById('telling-words-editor').innerHTML);
-
-    /*console.log(locationsOfWords);*/
-    console.log(result);
 }
 
 function getAllIndexes(array, value) {
